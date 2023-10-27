ae5fc8d193006e92fb323ac6ef0542cf8beb356c
JITESH PANDURANG SHEWALE <57071101+Jitesh001@users.noreply.github.com>
2023-10-28 01:34:56 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jslg)_p%^)i6zxc$fvvv8p_2w0&lb&gkc+pp*4^vl_u(ak&8yi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
