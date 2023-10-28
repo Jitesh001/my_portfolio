34a992b3c759af26b584a663073a9b067bede177
JITESH PANDURANG SHEWALE <57071101+Jitesh001@users.noreply.github.com>
2023-10-28 14:06:28 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Dg5dEd2-a54-C5BdDBdG5544b3FDG*eE',
+        'HOST' : 'viaduct.proxy.rlwy.net',
+        'PORT' : '31873',
     }
 }
 
